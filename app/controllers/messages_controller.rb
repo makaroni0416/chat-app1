2e381950cd336d2db116d7e3e5a841e92935fc8d
makaroni0416 <76754228+makaroni0416@users.noreply.github.com>
2021-01-21 11:28:05 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @new = Room.find(params[:room_id])
+    @message = @room.message.includes(:user)
   end
 
   def create
@@ -10,6 +11,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @message = @room.message.includes(:user)
       render :index
     end
 
