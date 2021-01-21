9eb07bbfc7f879fbaea773fca793687368776157
makaroni0416 <76754228+makaroni0416@users.noreply.github.com>
2021-01-21 22:54:02 +0900

    空のバリデーションが送信できないバリデーションの設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,21 +1,19 @@
 class MessagesController < ApplicationController
   def index
     @message = Message.new
-    @new = Room.find(params[:room_id])
-    @message = @room.message.includes(:user)
+    @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
     @room = Room.find(params[:room_id])
-    @message = @room.message.new(message_params)
+    @message = @room.messages.new(message_params)
     if @message.save
       redirect_to room_messages_path(@room)
     else
-      @message = @room.message.includes(:user)
+      @messages = @room.messages.includes(:user)
       render :index
     end
-
-    @message.save
   end
 
 private
--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module ChatApp1
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defalts 6.0
+    config.load_defaults 6.0 
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.generators do |g|
